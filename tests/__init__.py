4659d84f8a813d4008817df785b17fef74581ae5
Denis Shulyaka <Shulyaka@gmail.com>
2023-03-30 21:52:56 +0300

    Speed up tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,4 @@ import sys
 sys.path.append("tests/modules")
 sys.path.append("flash")
 sys.modules["time"] = __import__("mock_time")
+sys.modules["gc"] = __import__("mock_gc")
---
+++ tests/modules/mock_gc.py
@@ -0,0 +1,5 @@
+"""The gc mock module to speed up the tests."""
+
+from unittest.mock import MagicMock
+
+collect = MagicMock()
