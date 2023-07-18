9d17e8d6ef36eaf9be9f6f684605bf2f42068e6a
Denis Shulyaka <Shulyaka@gmail.com>
2023-07-18 16:17:47 +0300

    keep always initializing binary entities

--- flash/lib/core.py
+++ flash/lib/core.py
@@ -32,7 +32,10 @@ class Sensor:
             self._lowpass = lowpass if lowpass != 0 else None
         if period is not None:
             self._period = period if period != 0 else None
-        if not self._readonly and value is not None:
+        if not self._readonly and (
+            (self._type is None and value is not None)
+            or (self._type is not None and self._type(value) is not None)
+        ):
             self.state = value
 
         self.update()
