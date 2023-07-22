e2d26b8b966ccee7343f00dd7c960f58ad44db17
Denis Shulyaka <Shulyaka@gmail.com>
2023-07-23 01:38:20 +0300

    fix tests

--- tests_ha/test_sensor.py
+++ tests_ha/test_sensor.py
@@ -8,18 +8,20 @@ ENTITY2 = "sensor.xbee_humidifier_main_unit_pressure_in"
 
 
 @pytest.mark.parametrize(
-    "entity, data, value",
+    "entity, data, value, newdata, newvalue",
     (
-        (ENTITY1, "pump_temp", 31),
-        (ENTITY2, "pressure_in", 7),
+        (ENTITY1, "pump_temp", "31", 32, "32"),
+        (ENTITY2, "pressure_in", "7", 4158, "7.59986979166667"),
     ),
 )
-async def test_sensor(hass, data_from_device, test_config_entry, entity, data, value):
+async def test_sensor(
+    hass, data_from_device, test_config_entry, entity, data, value, newdata, newvalue
+):
     """Test sensor platform."""
 
-    assert hass.states.get(entity).state == str(value)
+    assert hass.states.get(entity).state == value
 
-    data_from_device(hass, IEEE, {data: value + 1})
+    data_from_device(hass, IEEE, {data: newdata})
     await hass.async_block_till_done()
 
-    assert hass.states.get(entity).state == str(value + 1)
+    assert hass.states.get(entity).state == newvalue
