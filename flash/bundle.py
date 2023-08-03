3630f28c28ded0b17cf4062f4ae5888694ac9a35
Denis Shulyaka <Shulyaka@gmail.com>
2023-08-03 20:23:01 +0300

    don't delete files in bundle to allow fallback to non-bundled

--- flash/bundle.py
+++ flash/bundle.py
@@ -7,7 +7,7 @@ import uos
 from micropython import opt_level
 from xbee import atcmd
 
-bundle_list = [
+_bundle_list = [
     "lib/logging.mpy",
     "lib/core.mpy",
     "lib/mainloop.mpy",
@@ -21,14 +21,14 @@ bundle_list = [
 
 atcmd("AP", 0)
 
-all_compiled = True
+_all_compiled = True
 for file in uos.listdir("/flash") + uos.listdir("/flash/lib"):
     if file[-3:] == ".py":
-        all_compiled = False
+        _all_compiled = False
         break
 
-if not all_compiled:
-    # First stage: compile files
+# First stage: compile files
+if not _all_compiled:
     opt_level(3)
 
     def compile_file(name):
@@ -54,10 +54,12 @@ if not all_compiled:
             compile_file("/flash/main.py")
         if "bundle.py" in uos.listdir("/flash"):
             compile_file("/flash/bundle.py")
+            if "bundle" in uos.bundle():
+                uos.bundle(None)
         # First bundle bundle.mpy
         if "bundle.mpy" in uos.listdir("/flash") and "bundle" not in uos.bundle():
             collect()
-            uos.bundle("bundle.mpy")
+            uos.bundle("bundle.mpy")  # This will trigger soft reset!
         compile_dir("/flash/lib")
         compile_dir("/flash")
         uos.sync()
@@ -67,31 +69,18 @@ if not all_compiled:
         uos.sync()
         machine.soft_reset()  # Retry after reboot
 
-    # Second stage: bundle
-    uos.mkdir(".bundle.tmp")
-    uos.sync()
-    collect()
-    uos.bundle(*bundle_list)  # This will trigger soft reset!
-
-else:
-    # Retry the bundle if needed
-    uos.chdir("/flash")
-    if ".bundle.tmp" not in uos.listdir("/flash"):
-        uos.mkdir(".bundle.tmp")
-        uos.sync()
-        collect()
-        uos.bundle("bundle.mpy")
-
-    if len(uos.bundle()) != len(bundle_list):
-        collect()
-        uos.bundle(*bundle_list)
-
-    # Third stage: delete bundled files
-    for file in uos.bundle() + ["bundle"]:
-        try:
-            uos.remove(file + ".mpy")
-        except OSError:
-            pass
-    uos.rmdir(".bundle.tmp")
+    uos.remove("bundle.mpy")
     uos.sync()
     machine.soft_reset()
+
+# Second stage: bundle
+if "bundle" not in uos.bundle():
+    uos.bundle("bundle.mpy")
+
+if "bundle.mpy" in uos.listdir("/flash"):
+    uos.remove("bundle.mpy")
+    uos.sync()
+    machine.soft_reset()
+
+collect()
+uos.bundle(*_bundle_list)
