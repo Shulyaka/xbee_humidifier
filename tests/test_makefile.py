9f3ba3e7f817aeafbbdbf2202817641aba78fe39
Denis Shulyaka <Shulyaka@gmail.com>
2023-08-04 02:34:43 +0300

    fix path to mpy-cross

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -2,6 +2,12 @@
 
 import os
 
+import mpy_cross
+
+_mpy_cross_path = os.path.dirname(mpy_cross.mpy_cross)
+if _mpy_cross_path not in os.environ["PATH"]:
+    os.environ["PATH"] = os.environ["PATH"] + ":" + _mpy_cross_path
+
 
 def test_compile():
     """Test compilation."""
