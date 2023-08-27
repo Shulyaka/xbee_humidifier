d0427bc223507980606910c55054da9d87cfe3b0
Denis Shulyaka <Shulyaka@gmail.com>
2023-08-27 16:18:15 +0300

    test compile in a temp dir

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -1,6 +1,7 @@
 """Test Makefile."""
 
 import os
+import tempfile
 
 import mpy_cross
 
@@ -9,13 +10,10 @@ if _mpy_cross_path not in os.environ["PATH"]:
     os.environ["PATH"] = os.environ["PATH"] + ":" + _mpy_cross_path
 
 
-def test_compile():
+def test_make():
     """Test compilation."""
 
-    assert os.system("make -C flash") == 0
-
-
-def test_clean():
-    """Test clean-up."""
-
-    assert os.system("make -C flash clean") == 0
+    with tempfile.TemporaryDirectory() as tmpdir:
+        os.system(f"cp -r flash/* {tmpdir}")
+        assert os.system(f"make -C {tmpdir}") == 0
+        assert os.system(f"make -C {tmpdir} clean") == 0
