Shulyaka/xbee_humidifier

commit ae7287caf7fb797d7f0220ff2d9c5340ee5c5392

Remove 'Main loop started' message as a trigger for reload

custom_components/xbee_humidifier/coordinator.py
@@ -238,7 +238,7 @@ class XBeeHumidifierDataUpdateCoordinator(DataUpdateCoordinator):
 
         async def async_log(data):
             self._xbee_logger.log(data["sev"], data["msg"])
-            if data["msg"] in ("Not initialized", "Main loop started"):
+            if data["msg"] == "Not initialized":
                 await self.async_request_refresh()
 
         self._remove_log_handler = self.client.add_subscriber("log", async_log)

custom_components/xbee_humidifier/humidifier.py
@@ -130,7 +130,7 @@ class XBeeHumidifier(XBeeHumidifierEntity, HumidifierEntity, RestoreEntity):
         await self._async_startup()  # init the sensor
 
         async def async_log(data):
-            if data["msg"] in ("Not initialized", "Main loop started"):
+            if data["msg"] == "Not initialized":
                 await self._async_startup(device_restarted=True)
 
         self.async_on_remove(self.coordinator.client.add_subscriber("log", async_log))

commit 708c88befea0cfae9a121e991c9449144fc6c558

Add 'Main loop started' message back as a trigger for reload (regression from ae7287c)

custom_components/xbee_humidifier/coordinator.py
@@ -238,7 +238,7 @@ class XBeeHumidifierDataUpdateCoordinator(DataUpdateCoordinator):
 
         async def async_log(data):
             self._xbee_logger.log(data["sev"], data["msg"])
-            if data["msg"] == "Not initialized":
+            if data["msg"] in ("Not initialized", "Main loop started"):
                 await self._subscribe()
 
         self._remove_log_handler = self.client.add_subscriber("log", async_log)

custom_components/xbee_humidifier/humidifier.py
@@ -160,7 +160,7 @@ class XBeeHumidifier(XBeeHumidifierEntity, HumidifierEntity, RestoreEntity):
                 self.async_on_remove(self._remove_sensor_tracking)
 
         async def async_log(data):
-            if data["msg"] == "Not initialized":
+            if data["msg"] in ("Not initialized", "Main loop started"):
                 await self._update_device()
 
         self.async_on_remove(self.coordinator.client.add_subscriber("log", async_log))

setup.cfg
@@ -91,7 +91,7 @@ ignore =
     D213
 
 [pyupgrade]
-py37plus = True
+py311plus = True
 
 [codespell]
 exclude = setup.cfg

tests_ha/test_coordinator.py
@@ -184,7 +184,7 @@ async def test_timeout(cmd_mock, hass):
 
     client = XBeeHumidifierApiClient(hass, IEEE)
 
-    cmd_mock.side_effect = asyncio.TimeoutError
+    cmd_mock.side_effect = TimeoutError
 
     with pytest.raises(TimeoutError, match="No response to bind command"):
         await client.async_command("bind")