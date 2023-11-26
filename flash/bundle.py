af1dfdf4ccd4fdfb3ea6593b52513d5f0f7751fe
Denis Shulyaka <Shulyaka@gmail.com>
2023-11-26 17:44:06 +0300

    Update bundle list

--- flash/bundle.py
+++ flash/bundle.py
@@ -13,10 +13,10 @@ _bundle_list = [
     "lib/mainloop.mpy",
     "lib/xbeepin.mpy",
     "tosr0x.mpy",
-    "tosr.mpy",
     "humidifier.mpy",
     "dutycycle.mpy",
     "commands.mpy",
+    "config.mpy",
 ]
 
 atcmd("AP", 0)
